3e452bdaafff95ab780ef3ee246882767fbeefda
Vaughan Reid <vaunus@gmail.com>
2017-03-07 15:12:24 +0000

    Updating pod template to Swift 3 and our current pod format

---
+++ templates/swift/PROJECT/Classes/PROJECTViewController.swift
@@ -0,0 +1,18 @@
+//
+//  PROJECTViewController.swift
+//  PROJECT
+//
+//  Created by Vaughan Reid on 07/03/2017.
+//  Copyright © 2017 CocoaPods. All rights reserved.
+//
+
+import UIKit
+
+public class PROJECTViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        //do stuff
+    }
+}
